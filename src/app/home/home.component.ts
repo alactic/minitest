3532830c7922d5297ed88ceb3b2f408341985f85
alactic <okaforechezona1992@gmail.com>
2020-05-14 20:39:24 +0100

    adding validation to the input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,17 +20,21 @@ export class HomeComponent implements OnInit {
   }
 
   validateForm() {
-    this.errorMessage = ''
-    if (this.x1 < 0 || this.x1 > 10000) {
+    this.errorMessage = '';
+    if (!this.x1 && !this.x1 && !this.x2 && !this.v2) {
+      this.errorMessage = "Please, fill all the fields"
+    }else if (this.x1 < 0 || this.x1 > 10000) {
       this.errorMessage = "x1 must be in the range of 0 and 10,000"
     } else if (this.x2 < 0 || this.x2 > 10000) {
-      this.errorMessage = "x2 must be in the range of 0 and 10,000"
+      this.errorMessage = `x2 must be in the range of ${+this.x1 + 1} and 10,000`
     } else if (this.x2 <= this.x1) {
-      this.errorMessage = "x1 must be less than x2"
+      this.errorMessage = "x2 must be greater than x1"
     } else if (this.v1 < 1 || this.v1 > 10000) {
       this.errorMessage = "v1 must be in the range of 1 and 10,000"
     } else if (this.v2 < 1 || this.v2 > 10000) {
       this.errorMessage = "v1 must be in the range of 1 and 10,000"
+    } else if (this.x1 === null || !this.v1 === null || !this.x2  === null || !this.v2 === null) {
+      this.errorMessage = "Please, fill all the fields"
     }
   }
 
@@ -43,19 +47,22 @@ export class HomeComponent implements OnInit {
   }
 
   submit() {
-    if (this.v2 > this.v1) {
-      this.result = 'NO'
-    } else {
-      const jumbs = this.x2 - this.x1;
-      const finalPoint_x1 = this.v1 * jumbs
-      const finalPoint_x2 = this.v2 * jumbs + (this.x2 - this.x1)
-      this.result = finalPoint_x1 === finalPoint_x2 ? "YES" : "NO";
+    this.validateForm()
+    if (this.errorMessage === '') {
+      if (this.v2 > this.v1) {
+        this.result = 'NO'
+      } else {
+        const jumbs = this.x2 - this.x1;
+        const finalPoint_x1 = this.v1 * jumbs
+        const finalPoint_x2 = this.v2 * jumbs + (this.x2 - this.x1)
+        this.result = finalPoint_x1 === finalPoint_x2 ? "YES" : "NO";
+      }
+      this.historyService.history.push({
+        date: new Date(),
+        fields: { x1: this.x1, x2: this.x2, v1: this.v1, v2: this.v2 },
+        result: this.result
+      })
+      console.log("result :: ", this.result)
     }
-    this.historyService.history.push({
-      date: new Date(),
-      fields: { x1: this.x1, x2: this.x2, v1: this.v1, v2: this.v2 },
-      result: this.result
-    })
-    console.log("result :: ", this.result)
   }
 }
